58befb0ddfbed7755e456f2804fec14c778d0356
Joao Costa <joao4.cost@luizalabs.com>
2026-02-09 12:40:07 -0300

    Deploy v3

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-const version = '1';
+const version = '3';
 
 app.get('/', (req, res) => {
   res.json({ version: version });
