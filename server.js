e14b913894ba43d0c289ad59d9a50f0340575203
Joao Costa <joao4.cost@luizalabs.com>
2026-02-09 12:31:03 -0300

    Deploy v2

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ app.get('/', (req, res) => {
   res.json({ version: version });
 });
 
+app.get('/health', (req, res) => {
+  res.send('OK');
+});
+
 app.listen(port, () => {
   console.log(`Argo Canary app listening on port ${port}, version ${version}`);
 });
